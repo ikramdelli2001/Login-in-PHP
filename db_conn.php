3bc9714f7f3441038d071a5d4484e7fc1ffd1c0a
ikramdelli2001 <karouma0delli2001@gmail.com>
2024-08-31 00:53:24 +0100

    init

---
+++ db_conn.php
@@ -0,0 +1,16 @@
+<?php 
+$sname="Localhost";
+$unmae="root";
+$password="";
+
+$db_name="test_db";
+
+
+$conn=mysqli_connect($sname,$unmae,$password,$db_name);
+
+
+
+if (!$conn) {
+   echo "mauvaise connection"; 
+}
+?>
